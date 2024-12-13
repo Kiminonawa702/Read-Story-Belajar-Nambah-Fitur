e7d2d542ad11ccc46d3330e57e62f800833c81f8
Kiminonawa702 <w.hatsagan@gmail.com>
2024-12-13 21:49:55 +0700

    Add files via upload

---
+++ BAGIAN 2/settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+  autoTyping: true, // Set to true to enable auto typing
+  uptime: true, // Set to true to enable uptime tracking, false to disable
+  updateBio: true, // Set to true to enable bio update with uptime, false to disable
+  notificationNumber: '6289688206739@s.whatsapp.net' // Add notification number with @s.whatsapp.net
+};
